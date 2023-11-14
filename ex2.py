45f8cf6ae8fab1c836d9e8d7264bea720c8941e0
PerrineLVB <perrine.launay@wanadoo.fr>
2023-11-14 15:21:01 +0100

    first commit

---
+++ ex2.py
@@ -0,0 +1,10 @@
+phrase = input ('Merci de saisir une phrase :')
+
+phraseMaj = phrase.upper()
+print (phraseMaj)
+
+phraseMin = phrase.lower()
+print (phraseMin)
+
+nbWords = len (phrase.split())
+print (nbWords)
