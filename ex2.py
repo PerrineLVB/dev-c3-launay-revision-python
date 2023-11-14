61f0c345c4f179bf2c4eb423ddfbf1182103056c
PerrineLVB <perrine.launay@wanadoo.fr>
2023-11-14 16:20:35 +0100

    ex2 - done et amélioré

--- ex2.py
+++ ex2.py
@@ -1,10 +1,11 @@
 phrase = input ('Merci de saisir une phrase :')
 
-phraseMaj = phrase.upper()
-print (phraseMaj)
-
-phraseMin = phrase.lower()
-print (phraseMin)
-
-nbWords = len (phrase.split())
-print (nbWords)
+if phrase.isdigit():
+    print("Votre phase ne contient que des nombres")
+else:
+    phraseMaj = phrase.upper()
+    print (phraseMaj)
+    phraseMin = phrase.lower()
+    print (phraseMin)
+    nbWords = len (phrase.split())
+    print (nbWords)
