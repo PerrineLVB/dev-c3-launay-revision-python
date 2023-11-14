f55f12237c7fa3ecf6a3ee33a1233d8c4319f818
PerrineLVB <perrine.launay@wanadoo.fr>
2023-11-14 19:27:35 +0100

    ex3 - gestion multiplication et division

--- exo/ex3.py
+++ exo/ex3.py
@@ -13,3 +13,20 @@ def evaluate_expression(expr):
             return "Caractère invalide : " + str(char)
     if current_number:
         elements.append(current_number)
+        
+    i = 0
+    while i < len(elements):
+        if elements[i] in ('*', '/'):
+            operator = elements[i]
+            leftOp = int(elements[i - 1])
+            rightOp = int(elements[i + 1])
+
+            if operator == '*':
+                elements[i - 1:i + 2] = [str(leftOp * rightOp)]
+            else:
+                if rightOp != 0:
+                    elements[i - 1:i + 2] = [str(leftOp / rightOp)]
+                else:
+                    return "Division par zéro impossible"
+        else:
+            i += 1
