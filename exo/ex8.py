53732062e23814049c0059d565444d3677b1564f
PerrineLVB <perrine.launay@wanadoo.fr>
2023-11-14 19:51:33 +0100

    ex8 - liste triée par notes décroissantes

---
+++ exo/ex8.py
@@ -0,0 +1,12 @@
+student_grades = {
+    'Antoine Dupont': 19,
+    'Romain Ntamack': 18,
+    'Thomas Ramos': 17,
+    'François Cros': 15,
+    'Julien Marchand': 16,
+    'Melvyn Jaminet': 8,
+    'Théo Ntamack': 13
+}
+
+student_grades_ordered = dict(sorted(student_grades.items(), key=lambda item: item[1], reverse=True))
+print(student_grades_ordered)
