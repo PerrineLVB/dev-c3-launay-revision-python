993e6e95168ad4b2aa57b40145a04fdbc2bc9a81
PerrineLVB <perrine.launay@wanadoo.fr>
2023-12-05 17:29:26 +0100

    ex8 - done

--- exo/ex8.py
+++ exo/ex8.py
@@ -9,4 +9,6 @@ student_grades = {
 }
 
 student_grades_ordered = dict(sorted(student_grades.items(), key=lambda item: item[1], reverse=True))
-print(student_grades_ordered)
+#print(student_grades_ordered)
+
+print(list(student_grades_ordered.keys())[0])
